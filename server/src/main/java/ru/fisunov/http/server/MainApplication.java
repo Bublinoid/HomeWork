918db639af649e8a39d9d815a6f049facfb3bec7
Hleb <gleb.qb@gmail.com>
2024-01-11 11:30:27 +0100

    home work 23 is ready

--- server/src/main/java/ru/fisunov/http/server/MainApplication.java
+++ server/src/main/java/ru/fisunov/http/server/MainApplication.java
@@ -1,14 +1,22 @@
 package ru.fisunov.http.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class MainApplication {
     public static final int PORT = 8189;
+    private static final int THREAD_POOL_SIZE = 10;
+    private static final Logger logger = LogManager.getLogger(MainApplication.class);
 
     // + К домашнему задания:
     // Добавить логирование!!!
@@ -18,31 +26,50 @@ public class MainApplication {
             Map<String, MyWebApplication> router = new HashMap<>();
             router.put("/calculator", new CalculatorWebApplication());
             router.put("/greetings", new GreetingsWebApplication());
-            System.out.println("Сервер запущен, порт: " + PORT);
-            try (Socket socket = serverSocket.accept()) {
-                System.out.println("Клиент подключился");
-                byte[] buffer = new byte[2048];
-                int n = socket.getInputStream().read(buffer);
-                String rawRequest = new String(buffer, 0, n);
-                Request request = new Request(rawRequest);
-                System.out.println("Получен запрос:");
-                request.show();
-                boolean executed = false;
-                for (Map.Entry<String, MyWebApplication> e : router.entrySet()) {
-                    if (request.getUri().startsWith(e.getKey())) {
-                        e.getValue().execute(request, socket.getOutputStream());
-                        executed = true;
-                        break;
-                    }
+            logger.info("Сервер запущен на порт: {}", PORT);
+
+            ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    logger.info("Клиент подключился");
+
+                    executorService.execute(() -> handleRequest(socket, router));
+
+                } catch (IOException e) {
+                   logger.error("Ошибка при соединении", e);
                 }
-                if (!executed) {
-                    socket.getOutputStream().write(("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Unknown application</h1></body></html>").getBytes(StandardCharsets.UTF_8));
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Ошибка при создании ServerSocket", e);
+        }
+    }
+
+    private static void handleRequest(Socket socket, Map<String, MyWebApplication> router) {
+
+        try {
+            byte[] buffer = new byte[2048];
+            int n = socket.getInputStream().read(buffer);
+            String rawRequest = new String(buffer, 0, n);
+            Request request = new Request(rawRequest);
+            System.out.println("Получен запрос:");
+            request.show();
+            boolean executed = false;
+            for (Map.Entry<String, MyWebApplication> e : router.entrySet()) {
+                if (request.getUri().startsWith(e.getKey())) {
+                    e.getValue().execute(request, socket.getOutputStream());
+                    executed = true;
+                    break;
+                }
+            }
+            if (!executed) {
+                socket.getOutputStream().write(("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Unknown application</h1></body></html>").getBytes(StandardCharsets.UTF_8));
+            }
+            socket.close();
+        } catch (IOException e) {
+            logger.error("Ошибка при создании запроса", e);
         }
     }
 }
+
